94cd30c228d5ff58f28cb1a51e68511490de923f
Nicholas Esposito <nesposito@ufl.edu>
2022-07-17 14:14:09 -0400

    Exploring trends with cleaned dataset

--- nespo08/Exploring-Yearly-Trends-Nick.R
+++ nespo08/Exploring-Yearly-Trends-Nick.R
@@ -1,43 +1,27 @@
----
-title: "Baseball-R-Dataset"
-author: "Nicholas Esposito and Amber Potter"
-date: "7/6/2022"
-output: html_document
----
+# PURPOSE: Explore data
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
 
-# Load Dataset
+# Load libraries and dataset ----------------------------------------------
 
-```{r}
 library(baseballr)
 library(tidyverse)
-
 statcast_data_all <- read_rds("data-nick/statcast_data_clean.rds")
-```
-## TEMP: Filter for 2020-22
 
-```{r}
+
+# TEMP: Filter for 2020-22 ------------------------------------------------
+
 statcast_data_2020_to_22 <- statcast_data_all %>%
   filter(game_date >= "2020-04-01")
-```
 
-
-## Counts and Percentages of Sample
-
-```{r}
-statcast_data_all %>%
+# Counts and percentages of variables by shift type and pitcher/batter matchups
+statcast_data_2020_to_22 %>%
   filter(!if_fielding_alignment == "", !of_fielding_alignment == "") %>%
   #select(stand, p_throws, if_fielding_alignment, of_fielding_alignment) %>%
   count(stand, p_throws, if_fielding_alignment, of_fielding_alignment) %>%
   mutate(prob = n/sum(n))
-```
 
-
-```{r}
-statcast_data_batters %>%
+# Counts and percentages of variables by combination of shift type and pitcher/batter matchups
+statcast_data_2020_to_22 %>%
   filter(!if_fielding_alignment == "", !of_fielding_alignment == "") %>%
   mutate(if_fielding_alignment = case_when(if_fielding_alignment == "Infield shift" ~ "Shift",
                                            if_fielding_alignment == "Strategic" ~ "Shift",
@@ -48,21 +32,15 @@ statcast_data_batters %>%
   #select(stand, p_throws, if_fielding_alignment, of_fielding_alignment) %>%
   count(stand, p_throws, if_fielding_alignment, of_fielding_alignment) %>%
   mutate(prob = n/sum(n))
-```
 
-```{r}
-statcast_data_batters %>%
+# Table by pitcher/batter matchups (regardless of shift type)
+statcast_data_2020_to_22 %>%
   #select(stand, p_throws, if_fielding_alignment, of_fielding_alignment) %>%
   count(stand, p_throws) %>%
   mutate(prob = n/sum(n))
-```
 
-## Prelim. Spray Charts
-
-```{r}
-statcast_data_batters %>%
+# Prelim. spray charts
+statcast_data_2020_to_22 %>%
   filter(stand == "R", p_throws == "L") %>%
   ggspraychart("hc_x", "-hc_y", bin_size = 100, point_alpha = 0.25, frame = TRUE) +
   facet_grid()
-```
-
